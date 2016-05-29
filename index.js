9836139942870fbe924cb977ee48d17dfe80298c
winsandymyint <winsandymyint@amdon.com>
2016-05-29 14:40:17 +0630

    Add the socket.io.js in frontend, write disconnecting function in index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app= require('express')();
 var http= require('http').Server(app);
+var io= require('socket.io')(http);
 
 app.get('/', function(req, res){
   //res.send('<h1>Hello word</h1>');
   res.sendFile(__dirname+ '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.info('A user is trying to connection ... ');
+  socket.on('disconnect', function(){ console.log('user disconnected')});
 });
 
 http.listen(9000, function(){
